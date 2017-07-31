ac15cc291fd5bde467a0a2b7f2d739019084f1f9
thibault <thibault.finot@gmail.com>
2017-07-31 20:11:25 +0000

    meteo with loop ok

--- meteo.rb
+++ meteo.rb
@@ -5,10 +5,23 @@ include PiPiper
 
 class Meteo
   def initialize
-    on_led = PiPiper::Pin.new(pin: 26, direction: :out)
-    sensor = DhtSensor.read(21, 11)
+    @on_led = PiPiper::Pin.new(pin: 26, direction: :out)
+    @sensor = DhtSensor.read(21, 11)
+    light(@on_led)
+    get_meteo
   end
 
-  puts sensor.temp               # => 21.899999618530273 (temp in C)
-  puts sensor.humidity           # => 22.700000762939453 (relative humidity %)
+  def get_meteo
+    while true do
+      puts @sensor.temp               # => 21.899999618530273 (temp in C)
+      puts @sensor.humidity           # => 22.700000762939453 (relative humidity %)
+      sleep(5) 
+   end
+  end
+
+  def light(led)
+    led.on
+  end
 end
+
+Meteo.new()
