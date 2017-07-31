0126a67c85339c74b2de5b906bd4df0f15c5d088
thibault <thibault.finot@gmail.com>
2017-07-31 23:43:28 +0200

    system with led ok

--- meteo.rb
+++ meteo.rb
@@ -1,27 +1,60 @@
 require 'rubygems'
 require "dht-sensor-ffi"
 require 'pi_piper'
+require 'byebug'
 include PiPiper
 
 class Meteo
   def initialize
     @on_led = PiPiper::Pin.new(pin: 26, direction: :out)
-    @sensor = DhtSensor.read(21, 11)
+    @white_led = PiPiper::Pin.new(pin: 5, direction: :out)
+    @yellow_led =  PiPiper::Pin.new(pin: 6, direction: :out)
+    @red_led = PiPiper::Pin.new(pin: 13, direction: :out)
     light(@on_led)
     get_meteo
   end
 
   def get_meteo
     while true do
-      puts @sensor.temp               # => 21.899999618530273 (temp in C)
-      puts @sensor.humidity           # => 22.700000762939453 (relative humidity %)
-      sleep(5) 
+      @sensor = DhtSensor.read(21, 11)
+      temp = @sensor.temp
+      humidity = @sensor.humidity
+      print_meteo(temp, humidity)
+      wich_led_to_light(temp)
+      sleep(5)
    end
+  end
+
+  def print_meteo(temp, humidity)
+    puts "Temperature = #{temp}°c; Humidité = #{humidity}%"
+  end
+
+  def wich_led_to_light(temp)
+    stop_all_led
+    case
+      when temp <= 15 then @white_led.on
+      when temp.between?(16,25) then @yellow_led.on
+      when temp > 25 then @red_led.on
+    end
   end
 
   def light(led)
     led.on
   end
+
+  def light_all_led
+    all_led.each { |led| led.on }
+  end
+
+  def stop_all_led
+    all_led.each { |led| led.off }
+  end
+
+  def all_led
+    [@white_led, @yellow_led, @red_led]
+  end
 end
 
-Meteo.new()
+meteo = Meteo.new()
+
+meteo.wich_led_to_light(60)
---
+++ readme.md
@@ -0,0 +1,15 @@
+Ruby Meteo Station
+
+To play with raspberry PI.
+
+Need :
+- rvm ttps://rvm.io/
+- bcm2835 lib : http://www.airspayce.com/mikem/bcm2835/
+
+First install bundle
+`gem install bundle`
+
+Then run `bundle install`
+
+And to run the project :
+`rvmsudo meteo.rb`
