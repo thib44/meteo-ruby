cd7a6602fa9d632e4c1b73af885cdfc9c241a0a7
thibault <thibault.finot@gmail.com>
2017-07-31 19:56:31 +0000

    add pipiper and start with led

--- meteo.rb
+++ meteo.rb
@@ -1,4 +1,11 @@
+require 'rubygems'
 require "dht-sensor-ffi"
+require 'pi_piper'
+include PiPiper
+
+led = PiPiper::Pin.new(pin: 26, direction: :out)
+
+led.on
 val = DhtSensor.read(21, 11) # pin=21, sensor type=DHT-11
 puts val.temp               # => 21.899999618530273 (temp in C)
 puts val.humidity           # => 22.700000762939453 (relative humidity %)
