a3e95fea876f593115e9b913c6b77c7a443d85fb
thibault <thibault.finot@gmail.com>
2017-08-07 19:30:09 +0200

    take meteo ok

--- meteo.rb
+++ meteo.rb
@@ -5,7 +5,13 @@ require './temperature_pressure_sensor.rb'
 #require 'byebug'
 include PiPiper
 
+require 'net/http'
+require 'json'
+
 class Meteo
+
+  URL = 'http://55ddfe79.ngrok.io/meteos'
+
   def initialize
     @on_led = PiPiper::Pin.new(pin: 26, direction: :out)
     @white_led = PiPiper::Pin.new(pin: 5, direction: :out)
@@ -24,9 +30,27 @@ class Meteo
       humidity = @sensor.humidity
       print_meteo(temp, humidity, temp_pres)
       wich_led_to_light(temp)
-      sleep(5)
+      send_meteo(temp_pres, humidity)
+      sleep(1800)
    end
   end
+
+  def send_meteo(temp_pres, humidity)
+    uri = URI(URL)
+    http = Net::HTTP.new(uri.host, uri.port)
+    # http.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+    req.body = { temperature: temp_pres.temp, pressure: temp_pres.pressure, humidity: humidity }.to_json
+    puts req.body
+    res = http.request(req)
+  end
+
+  #  uri = URI('http://api.nsa.gov:1337/agent')
+  #  http = Net::HTTP.new(uri.host, uri.port)
+  #  req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+  #  req.body = {name: 'John Doe', role: 'agent'}.to_json
+  #  res = http.request(req)
+  #  puts "response #{res.body}"
 
   def print_meteo(temp, humidity, temp_pres)
     puts "Temperature = #{temp}°c; Humidité = #{humidity}%, other captor: temp: #{temp_pres.temp / 10.0}, pres: #{temp_pres.pressure}"
@@ -59,7 +83,7 @@ class Meteo
 
   def get_temp_pres
     temp_pres = TemperaturePressureSensor.new("/dev/i2c-1")
-    tempPres.read(3) # highest accuracy
+    temp_pres.read(3) # highest accuracy
   end
 end
 
